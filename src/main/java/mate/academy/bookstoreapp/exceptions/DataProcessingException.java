be5bad1bce833e74cfcdfb4836b0db2695726b4b
Kateryna <keitkat98@gmail.com>
2024-06-08 11:26:55 +0300

    added BookDto and CreateBookRequestDto

---
+++ src/main/java/mate/academy/bookstoreapp/exceptions/DataProcessingException.java
@@ -0,0 +1,7 @@
+package mate.academy.bookstoreapp.exceptions;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

f7046796dca680745e6861b844d91837d4c50db4
Kateryna <keitkat98@gmail.com>
2024-06-10 10:01:53 +0300

    notBlank

--- src/main/java/mate/academy/bookstoreapp/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstoreapp/dto/CreateBookRequestDto.java
@@ -1,6 +1,6 @@
 package mate.academy.bookstoreapp.dto;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -8,9 +8,9 @@ import java.math.BigDecimal;
 import mate.academy.bookstoreapp.validation.Isbn;
 
 public record CreateBookRequestDto(
-        @NotEmpty String title,
-        @NotEmpty String author,
-        @NotNull @Isbn String isbn,
+        @NotBlank String title,
+        @NotBlank String author,
+        @NotBlank @Isbn String isbn,
         @NotNull @Positive BigDecimal price,
         @Size(max = 1000) String description,
         String coverImage
